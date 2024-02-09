7da55aa3c5c73a13a3f9f886b2e64c1c357e663d
danilo012 <46241643+danilo012@users.noreply.github.com>
2024-02-09 23:41:08 +0800

    Update Header.js

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -46,7 +46,7 @@ const Header = (props) => {
 
       <div className={`balance ${!loggedIn ? 'd-none' : ''}`}>
         <span className="label">Balance</span>
-        <span className="value">€{balance.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
+        <span className="value">₱{balance.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
       </div>
     </div>
   );
